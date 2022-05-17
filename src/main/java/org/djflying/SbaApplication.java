2829facdf0c645f8a5a36616a36d87dd166358b9
daijiong <djflying1988@163.com>
2022-05-17 17:50:11 +0800

    test webhook

--- src/main/java/org/djflying/SbaApplication.java
+++ src/main/java/org/djflying/SbaApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SbaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SbaApplication.class, args);
     }
